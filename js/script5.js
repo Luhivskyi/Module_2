2ac7e7f64d350311e7a421b2654cea35dd11dfb4
Luhivskyi <luhivskyi.d@ukr.net>
2021-01-08 19:15:04 +0300

    Доступ к элементам по индексу  5
    Объяви три переменные и присвой каждой из них значение, используя нотацию квадратных скобок.
    
    Имя переменной	Значение переменной
    firstElement	первый элемент массива
    secondElement	второй элемент массива
    lastElement	последний элемент массива

---
+++ js/script5.js
@@ -0,0 +1,5 @@
+const fruits = ['яблоко', 'слива', 'груша', 'апельсин'];
+// Пиши код ниже этой строки
+const firstElement = 'яблоко';
+const secondElement = 'слива';
+const lastElement = 'апельсин';
