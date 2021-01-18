d1fc3aee9da6a835845ecebeadc5311c661fdb1c
Luhivskyi <luhivskyi.d@ukr.net>
2021-01-18 18:42:34 +0300

    Метод push( )  22
    Дополни код функции createArrayOfNumbers(min, nax) так, чтобы она возвращала массив всех целых чисел от значения min до max.

---
+++ js/script22.js
@@ -0,0 +1,12 @@
+function createArrayOfNumbers(min, max) {
+  const numbers = [];
+  // Пиши код ниже этой строки
+  
+   for(;min <= max; min++) {
+     
+  numbers.push(min);
+     
+   }
+  // Пиши код выше этой строки
+  return numbers;
+}
