b40a58d15594875ae89096f7a93cb5e46edfad0f
Luhivskyi <luhivskyi.d@ukr.net>
2021-01-22 09:53:19 +0300

    Цикл for...of  26
    Выполни рефакторинг кода функции calculateTotalPrice(order) заменив цикл for на for...of.

---
+++ js/script26.js
@@ -0,0 +1,11 @@
+function calculateTotalPrice(order) {
+  let total = 0;
+  // Пиши код ниже этой строки
+
+  for (const item of order) {
+   total += item;
+  }
+  
+return total;
+  // Пиши код выше этой строки
+} 
